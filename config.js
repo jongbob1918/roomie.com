42ca9432d5d3bf30e106538b98b402e0e6873f8a
Jongmyung Kim <jongbob1918@gmail.com>
2025-07-23 19:03:49 +0900

    4

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 프론트엔드 공통 설정 값 보관소
 const CONFIG = {
     // ❗️ 여기만 수정하면 모든 페이지의 서버 주소가 변경됩니다.
-    API_BASE_URL: "https://cf549201aecc.ngrok-free.app/api",
+    API_BASE_URL: "https://2b20e064bace.ngrok-free.app/api",
     //API_BASE_URL: "http://127.0.0.1:8000/api",
     DEFAULT_ROOM_ID: "201호"
 };
