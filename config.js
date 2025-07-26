658518ef1b7e9cb2aed1547bd21b04f03d044173
Jongmyung Kim <jongbob1918@gmail.com>
2025-07-26 19:57:26 +0900

    55

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 // 프론트엔드 공통 설정 값 보관소
-const CONFIG = {
-    API_BASE_URL: "https://7d28b7335ad0.ngrok-free.app/api",
-    WS_BASE_URL: "wss://7d28b7335ad0.ngrok-free.app/ws", // WebSocket 주소 추가
-    //API_BASE_URL: "http://127.0.0.1:8000/api", // 로컬 테스트용 (ngrok 사용 시 주석 처리)
-   //WS_BASE_URL: "ws://127.0.0.1:8000/ws", // 로컬 테스트용 (ngrok 사용 시 주석 처리)
-    DEFAULT_ROOM_ID: "201호"
+const CONFIG = 
+{
+    //API_BASE_URL: "https://7d28b7335ad0.ngrok-free.app/api",
+    //WS_BASE_URL: "wss://7d28b7335ad0.ngrok-free.app/ws", // WebSocket 주소 추가
+    API_BASE_URL: "http://192.168.0.47:8000/api/gui", // 로컬 테스트용 (ngrok 사용 시 주석 처리)
+    WS_BASE_URL: "ws://192.168.0.47:8000/api/gui/ws/guest/ROOM_102", // 로컬 테스트용 (ngrok 사용 시 주석 처리)
+    DEFAULT_ROOM_ID: "102호"
 };
 
